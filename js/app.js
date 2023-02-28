029e21f88f2c33a6873441d65c0f78176d25fa8a
Will Woods Ballard <willwoodsb@gmail.com>
2023-02-28 08:59:21 +0000

    fixed bug with scroll links

--- js/app.js
+++ js/app.js
@@ -114,15 +114,9 @@ function submitForm(userInput){
 //---------------------------------------------
 
 //scroll button animations
-$('.scroll, .internal-links').on('click', function(event) {
-    let time = 0;
-    if (event.target.classList.contains('internal-links')) {
-        time = 550;
-    }
+$('.internal-links').on('click', function(event) {
+    popoutState = true;
 
-    if ($(event.target).hasClass('internal-links')) {
-        popoutState = true;
-    } 
     setTimeout(function() {
         let target = $(event.target.hash);
         if (target) {
@@ -131,10 +125,22 @@ $('.scroll, .internal-links').on('click', function(event) {
                 scrollTop: target.offset().top
             }, 700);
         }
-    }, time);
+    }, 550);
     
 });
+//scroll button animations
+$('.scroll').on('click', function(event) {
 
+    let target = $(this.getAttribute('href'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 700);
+    }
+
+    
+});
 
 //scroll event listener
 window.addEventListener("scroll", function() {
